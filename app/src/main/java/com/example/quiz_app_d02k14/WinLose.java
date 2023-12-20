8b99aa568b98809885634a19ca5c9841222db586
hiep <tranduchiep312@gmail.com>
2023-12-20 22:04:00 +0700

    new interface

---
+++ app/src/main/java/com/example/quiz_app_d02k14/WinLose.java
@@ -0,0 +1,14 @@
+package com.example.quiz_app_d02k14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WinLose extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_win_lose);
+    }
+}
---
+++ app/src/main/java/com/example/quiz_app_d02k14/selectDifficulty.java
@@ -0,0 +1,14 @@
+package com.example.quiz_app_d02k14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class selectDifficulty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_difficulty);
+    }
+}
